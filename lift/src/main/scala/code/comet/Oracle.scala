e5b5e5309afb5f282e86a7fc8fff124926fbf353
Hans Speijer <hans.speijer@mindsteps.eu>
2011-06-04 21:28:47 +0200

    Initial checkin

---
+++ lift/src/main/scala/code/comet/Oracle.scala
@@ -0,0 +1,27 @@
+package code
+package comet
+
+import net.liftweb._
+import http._
+import actor._
+import collection.immutable.HashMap
+
+/**
+ * (c) mindsteps BV 
+ *
+ * User: Hans Speijer
+ * Date: 4-6-11
+ * Time: 2:35
+ * 
+ */
+
+object Oracle {
+  var state = 0
+  val msgs = Array("Earth", "Water", "Fire", "Air", "Aether")
+  val menu = "Choices are: 1: Earth, 2:Water, 3:Fire, 4:Air & 5 Aether";
+
+  def updateState(state: Int) : String = {
+    this.state = state
+    msgs(state - 1)
+  };
+}
---
+++ src/net/destinylounge/Oracle.scala
@@ -0,0 +1,16 @@
+package net.destinylounge
+
+/**
+ * (c) mindsteps BV 
+ *
+ * User: Hans Speijer
+ * Date: 25-5-11
+ * Time: 6:01
+ * 
+ */
+
+object Oracle {
+  def main(args: Array[String]) {
+      println("Hello, world!")
+  }
+}
