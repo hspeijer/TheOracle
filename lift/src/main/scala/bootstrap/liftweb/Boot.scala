f873ed25413bc16350b462fc4e1f8d05f1b74c69
Hans Speijer <hans.speijer@mindsteps.eu>
2011-07-18 01:49:40 +0200

    * JQGrid * rest Support * Protovis

--- lift/src/main/scala/bootstrap/liftweb/Boot.scala
+++ lift/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -2,6 +2,8 @@ package bootstrap.liftweb
 
 import net.liftweb._
 import sitemap.LocPath._
+import code.rest.OracleRest
+
 //import sitemap.MenuSingleton._
 import util._
 import common._
@@ -59,19 +61,21 @@ class Boot {
     // Use jQuery 1.4
     LiftRules.jsArtifacts = net.liftweb.http.js.jquery.JQuery14Artifacts
 
-    //Show the spinny image when an Ajax call starts
-    LiftRules.ajaxStart =
-      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
-    
-    // Make the spinny image go away when it ends
-    LiftRules.ajaxEnd =
-      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
+    LiftRules.dispatch.prepend(OracleRest.dispatch)
+
+//    //Show the spinny image when an Ajax call starts
+//    LiftRules.ajaxStart =
+//      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
+//
+//    // Make the spinny image go away when it ends
+//    LiftRules.ajaxEnd =
+//      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
 
     // Force the request to be UTF-8
     LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
 
-    // What is the function to test if a user is logged in?
-    LiftRules.loggedInTest = Full(() => User.loggedIn_?)
+//    // What is the function to test if a user is logged in?
+//    LiftRules.loggedInTest = Full(() => User.loggedIn_?)
 
     // Use HTML5 for rendering
     LiftRules.htmlProperties.default.set((r: Req) =>
---
+++ lift/src/main/scala/code/snippet/OracleData.scala
@@ -0,0 +1,47 @@
+package code.snippet
+
+import net.liftweb.http.SHtml
+import code.comet.OracleServer
+import net.liftweb.http.js.JsCmds.SetValById._
+import net.liftweb.http.js.JsExp._
+import net.liftweb.http.js.JE._
+import xml.NodeSeq
+
+
+/**
+ * (c) mindsteps BV 
+ *
+ * User: Hans Speijer
+ * Date: 17-7-11
+ * Time: 23:15
+ * 
+ */
+
+object OracleData {
+  import net.liftweb.json.JsonAST
+  import net.liftweb.json.JsonDSL._
+
+  case class Winner(id: Long, numbers: List[Int])
+  case class Lotto(id: Long, winningNumbers: List[Int], winners: List[Winner], drawDate: Option[java.util.Date])
+
+  val winners = List(Winner(23, List(2, 45, 34, 23, 3, 5)), Winner(54, List(52, 3, 12, 11, 18, 22)))
+  val lotto = Lotto(5, List(2, 45, 34, 23, 7, 5, 3), winners, None)
+
+  val json =
+    ("lotto" ->
+      ("lotto-id" -> lotto.id) ~
+      ("winning-numbers" -> lotto.winningNumbers) ~
+      ("draw-date" -> lotto.drawDate.map(_.toString)) ~
+      ("winners" ->
+        lotto.winners.map { w =>
+          (("winner-id" -> w.id) ~
+           ("numbers" -> w.numbers))}))
+
+  def render(in: NodeSeq) = {
+    JsRaw(json.toString()).cmd
+  }
+
+  def oracle(in: NodeSeq) : NodeSeq = {
+    <div>{JsonAST.render(json)}</div>
+  }
+}
