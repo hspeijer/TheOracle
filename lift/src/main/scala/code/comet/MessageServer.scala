9f8ad8fc05af481a9ce0ec06a0d27a3099c4e181
Hans Speijer <hspeijer@destiny.local>
2011-09-13 13:42:13 -0700

    Field repairs

--- lift/src/main/scala/code/comet/MessageServer.scala
+++ lift/src/main/scala/code/comet/MessageServer.scala
@@ -11,7 +11,7 @@ import net.destinylounge.oracle.Oracle
  * It's an Actor so it's thread-safe because only one
  * message will be processed at once.
  */
-object OracleServer extends LiftActor with ListenerManager {
+object MessageServer extends LiftActor with ListenerManager {
   private var msgs = Vector("Welcome to the Oracle") // private state
 
   msgs :+= Oracle.reset()
@@ -28,7 +28,7 @@ object OracleServer extends LiftActor with ListenerManager {
   /**
    * process messages that are sent to the Actor.  In
    * this case, we're looking for Strings that are sent
-   * to the OracleServer.  We append them to our Vector of
+   * to the MessageServer.  We append them to our Vector of
    * messages, and then update all the listeners.
    */
   override def lowPriority = {
